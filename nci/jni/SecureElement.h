f9b189dc2fcec8762fda5ba28c9e7c593b9f79d0
Houston Hoffman <hhoffman@codeaurora.org>
2014-01-08 18:06:48 -0800

    NFC: Add delay between SWP activation and the first APDU from DH
        Activate SWP by using proprietary NCI command and then send APDU
        Check UICC vendor to decide sending Warm Reset Cmd
    
    Change-Id: I710e3e098f26623361744327f1fcbaa9c95edb4e

--- nci/jni/SecureElement.h
+++ nci/jni/SecureElement.h
@@ -413,6 +413,7 @@ private:
     SyncEvent       mTransceiveEvent;
     SyncEvent       mVerInfoEvent;
     SyncEvent       mRegistryEvent;
+    SyncEvent       mHciSentEvent;
     UINT8           mVerInfo [3];
     UINT8           mResponseData [MAX_RESPONSE_SIZE];
     RouteDataSet    mRouteDataSet; //routing data
@@ -422,6 +423,8 @@ private:
     bool            mRfFieldIsOn; // last known RF field state
     struct timespec mLastRfFieldToggle; // last time RF field went off
     bool            mReceivedWtx; // true if received WTX request from UICC
+    bool            mReadingVendorName; // true if reading vendor name from identity management gate
+    char            mVendorName[21];    // UICC vendor name
     /*******************************************************************************
     **
     ** Function:        SecureElement
